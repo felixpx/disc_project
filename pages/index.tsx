75ac25c15355c2b0b88fb0a63bf89814b30fb191
Dominic Leon Hackett <dominichackett@gmail.com>
2023-02-02 15:12:45 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../src/components/Navbar";
 import Featured2 from "../src/components/Featured2";
-import { Profile } from "../src/components/Wallet/SignIn";
+import  {Profile}  from "../src/components/Wallet/SignIn";
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center">
